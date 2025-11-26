ed7fdda4583437f7a6177edfabb89e8e646e844d
asdev-flower <84808891+asdev-flower@users.noreply.github.com>
2025-11-26 02:45:08 -0500

    sqlpt3 studio finishing

--- sql-part-3/studio/sql-studio-coding.sql
+++ sql-part-3/studio/sql-studio-coding.sql
@@ -33,7 +33,7 @@ WHERE tags.tag_name like '%ireland%'
 OR tags.tag_name like '%irish%'
 OR tags.tag_name like '%luck%'
 OR tags.tag_name like '%Saint Patrick%';
-*/ 
+
 
 SELECT TOP(100) t.tag_id, COUNT(*) AS Times_Used, bt.tag_name
 FROM dbo.book_tags as t
@@ -42,3 +42,30 @@ ON t.tag_id = bt.tag_id
 GROUP BY t.tag_id, bt.tag_name
 HAVING bt.tag_name LIKE '%female%'
 ORDER BY Times_Used DESC
+*/ 
+
+SELECT TOP 20 SUM([count]) AS times_tagid_used,
+tags.tag_id,
+tags.tag_name,
+books.authors,
+books.average_rating,
+books.title
+
+FROM BooksDB.dbo.book_tags AS bts
+INNER JOIN BooksDB.dbo.tags AS tags
+ON tags.tag_id = bts.tag_id
+INNER JOIN BooksDB.dbo.books AS books
+ON books.book_id = bts.goodreads_book_id
+
+GROUP BY tags.tag_id, tags.tag_name, books.authors, books.average_rating, books.title
+
+HAVING tags.tag_name like 'college-football'
+OR tags.tag_name LIKE 'american-football' 
+--OR tags.tag_name like '%valentine%'
+--OR tags.tag_name like '%romance%'
+ORDER BY times_tagid_used DESC;
+
+SELECT *
+
+FROM tags
+WHERE tag_name LIKE 'college-football'
